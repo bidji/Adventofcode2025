d3ea4d88ae5c39655ccaa48b278126e22b499e58
David Morineau <d.morineau@artis.fr>
2025-12-02 09:31:28 +0100

    :art: improve code on day02

--- day02/solve.py
+++ day02/solve.py
@@ -10,22 +10,19 @@ class RangeId:
     def __str__(self):
         return f"first: '{self.first}', last: '{self.last}'"
     
-    def get_invalid_ids(self) -> list[int]:
+    def get_invalid_ids(self, version: int) -> list[int]:
         invalid_ids = []
         for i in range(self.first, self.last + 1):
-            id = str(i)
-            if len(id) % 2 == 0:
-                length = int(len(id) / 2)
-                if id[:length] == id[length:]:
-                    invalid_ids.append(i)
-        return invalid_ids
-    
-    def get_invalid_ids_v2(self) -> list[int]:
-        invalid_ids = []
-        for i in range(self.first, self.last + 1):
-            if RangeId.is_invalid_v2(str(i)):
+            if (version == 1 and RangeId.is_invalid(str(i))) or (version == 2 and RangeId.is_invalid_v2(str(i))):
                 invalid_ids.append(i)
         return invalid_ids
+    
+    def is_invalid(id: str) -> bool:
+        if len(id) % 2 == 0:
+            length = int(len(id) / 2)
+            if id[:length] == id[length:]:
+                return True
+        return False
     
     def is_invalid_v2(id: str) -> bool:
         length = int(len(id) / 2)
@@ -44,25 +41,18 @@ def get_ranges(filename: str) -> list[RangeId]:
 
 os.chdir('/home/dmorineau/Data/depots/perso/Adventofcode2025/day02')
 
-def sum_invalids(filename: str) -> int:
+def sum_invalids(filename: str, version: int) -> int:
     sum = 0
     for r in get_ranges(filename):
-        for i in r.get_invalid_ids():
-            sum += i
-    return sum
-
-def sum_invalids_v2(filename: str) -> int:
-    sum = 0
-    for r in get_ranges(filename):
-        for i in r.get_invalid_ids_v2():
+        for i in r.get_invalid_ids(version):
             sum += i
     return sum
 
 print("part 1:")
-print(sum_invalids('sample'))
-print(sum_invalids('input'))
+print(sum_invalids('sample', 1))
+print(sum_invalids('input', 1))
 
 print("\npart 2:")
-print(sum_invalids_v2('sample'))
-print(sum_invalids_v2('input'))
+print(sum_invalids('sample', 2))
+print(sum_invalids('input', 2))
         
