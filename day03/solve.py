435f509050a2dfbd44883ac0bb97ebc3572aeeae
David Morineau <d.morineau@artis.fr>
2025-12-03 11:51:32 +0100

    [day03] 2 stars

--- day03/solve.py
+++ day03/solve.py
@@ -1,3 +1,4 @@
+import math
 import os
 
 def get_banks(filename: str) -> list[str]:
@@ -7,31 +8,33 @@ def get_banks(filename: str) -> list[str]:
             banks.append(line.strip())
     return banks
 
-def get_max_voltage(bank: str) -> int:
-    maxi = 0
-    maxten = int(bank[maxi])
-    for i in range(1, len(bank) - 1):
-        if maxten < int(bank[i]):
-            maxi = i
-            maxten = int(bank[i])
-    
-    maxdigit = int(bank[maxi + 1])
-    for j in range(maxi + 1, len(bank)):
-        if maxdigit < int(bank[j]):
-            maxdigit = int(bank[j])
-    
-    return maxten * 10 + maxdigit
+def get_max_from_subbank(bank: str, start: int, truncate: int) -> int:
+    value = max(bank[start:-truncate]) if truncate > 0 else max(bank[start:])
+    return int(value), bank[start:].find(str(value)) + start
 
-def solve(filename: str) -> int:
+def get_max_voltage(bank: str, nb_digits: int) -> int:
+    voltage = 0
+    index = 0
+    for n in range(nb_digits):
+        value, pos = get_max_from_subbank(bank, index, nb_digits - 1 - n)
+        voltage += value * (10 ** (nb_digits - n - 1))
+        index = pos + 1
+    return voltage
+
+def solve(filename: str, nb_digits: int) -> int:
     voltage = 0
     
     for bank in get_banks(filename):
-        voltage += get_max_voltage(bank)
+        voltage += get_max_voltage(bank, nb_digits)
     
     return voltage
 
 os.chdir(os.path.expanduser('~/Data/depots/perso/Adventofcode2025/day03'))
 
 print("part 1:")
-print(solve('sample'))
-print(solve('input'))
+print(solve('sample', 2))
+print(solve('input', 2))
+
+print("part 2:")
+print(solve('sample', 12))
+print(solve('input', 12))
