8801681ab2b82acd2edb058cdfc0d60b714be0ce
David Morineau <d.morineau@artis.fr>
2025-12-08 11:38:14 +0100

    [day07] 2 stars

--- day07/solve.py
+++ day07/solve.py
@@ -1,10 +1,10 @@
 import os
 
-def get_manifold(filename: str) -> list[str]:
+def get_manifold(filename: str) -> list[list[str]]:
     manifold = []
     with open(filename, 'r') as f:
         for line in f.readlines():
-            manifold.append(line.strip().replace('S', '|'))
+            manifold.append([c for c in line.strip().replace('S', '1')])
     return manifold
 
 def part1(filename: str):
@@ -14,23 +14,49 @@ def part1(filename: str):
     splits = 0
     for num_line in range(1, len(manifold)):
         for num_col in range(width):
-            if manifold[num_line - 1][num_col] == '|':
-                # there is a | above
+            if manifold[num_line - 1][num_col] == '1':
+                # there is a beam above
                 if manifold[num_line][num_col] == '^':
                     # splitter
                     splits += 1
-                    if num_col == 0:
-                        manifold[num_line] = '^|' + manifold[num_line][2:]
-                    elif num_col == width - 1:
-                        manifold[num_line] = manifold[num_line][:-2] + '|^'
-                    else:
-                        manifold[num_line] = manifold[num_line][:num_col - 1] + '|^|' + manifold[num_line][num_col + 2:]
+                    if num_col > 0:
+                        manifold[num_line][num_col - 1] = '1'
+                    if num_col < width - 1:
+                        manifold[num_line][num_col + 1] = '1'
                 else:
                     # no splitter
-                    manifold[num_line] = manifold[num_line][:num_col] + '|' + manifold[num_line][num_col + 1:]
+                    manifold[num_line][num_col] = '1'
     
     return splits
-                    
+
+def part2(filename: str):
+    manifold = get_manifold(filename)
+    width = len(manifold[0])
+    
+    for num_line in range(1, len(manifold)):
+        for num_col in range(width):
+            if manifold[num_line - 1][num_col] != '.' and manifold[num_line - 1][num_col] != '^':
+                # there is a beam above
+                beam_size = int(manifold[num_line - 1][num_col])
+                if manifold[num_line][num_col] == '^':
+                    # splitter
+                    if num_col > 0:
+                        existing_beam = int(manifold[num_line][num_col - 1].replace('.', '0'))
+                        manifold[num_line][num_col - 1] = str(beam_size + existing_beam)
+                    if num_col < width - 1:
+                        existing_beam = int(manifold[num_line][num_col + 1].replace('.', '0'))
+                        manifold[num_line][num_col + 1] = str(beam_size + existing_beam)
+                else:
+                    # no splitter
+                    existing_beam = int(manifold[num_line][num_col].replace('.', '0'))
+                    manifold[num_line][num_col] = str(beam_size + existing_beam)
+
+    nb_beams = 0            
+    for c in manifold[-1]:
+        if c != '.' and c != '^':
+            nb_beams += int(c)
+    
+    return nb_beams
 
 os.chdir(os.path.expanduser('~/Data/depots/perso/Adventofcode2025/day07'))
 
@@ -39,3 +65,5 @@ print(part1('sample'))
 print(part1('input'))
 
 print("part 2:")
+print(part2('sample'))
+print(part2('input'))
